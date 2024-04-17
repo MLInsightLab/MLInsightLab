79aba251f45d4c8b72b2ae2bec67d691fabaa30f
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-04-17 15:56:02 -0400

    moving logic to create user to another location
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/launch.sh
+++ jupyter/launch.sh
@@ -9,12 +9,18 @@ fi
 # Check if the value of JUPYTER_ENV is "jupyterlab"
 if [ "$JUPYTER_ENV" = "jupyterlab" ]; then
     echo "JUPYTER_ENV is set to jupyterlab"
-    su odsp
     jupyter-lab --ip 0.0.0.0 --port 8000 --allow-root --notebook-dir=/home --ServerApp.token='' --ServerApp.password=''
 
 # Check if the value of JUPYTER_ENV is "jupyterhub"
 elif [ "$JUPYTER_ENV" = "jupyterhub" ]; then
     echo "JUPYTER_ENV is set to jupyterhub"
+    adduser --disabled-password --gecos "" odsp
+    adduser odsp sudo
+    
+    # Allow root and odsp users to run sudo commands without password
+    RUN echo "root ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/root
+    RUN echo "odsp ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/odsp
+    
     jupyterhub -f /srv/jupyterhub/jupyterhub_config.py --ip 0.0.0.0
 
 # Else, the value is not understood
