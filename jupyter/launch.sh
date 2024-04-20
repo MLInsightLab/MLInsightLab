859dbfae54b8d8bc1ec49e516b67aea825b4bf42
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-04-19 22:17:31 -0400

    creating odsp user regardless of jupyterhub or jupyterlab
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/launch.sh
+++ jupyter/launch.sh
@@ -6,25 +6,30 @@ if [ -z "$JUPYTER_ENV" ]; then
   exit 1
 fi
 
+# create odsp user
+adduser --disabled-password --gecos "" odsp
+adduser odsp sudo
+
+# Allow root and odsp users to run sudo commands without password
+echo "root ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/root
+echo "odsp ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/odsp
+
+# Add MLFLOW_TRACKING_URI and DASK_SCHEDULER_ADDRESS to odsp user
+echo "export MLFLOW_TRACKING_URI=http://mlflow:2244" >> /home/odsp/.bashrc
+echo "export DASK_SCHEDULER_ADDRESS=http://dask-scheduler:8786" >> /home/odsp/.bashrc
+
 # Check if the value of JUPYTER_ENV is "jupyterlab"
 if [ "$JUPYTER_ENV" = "jupyterlab" ]; then
     echo "JUPYTER_ENV is set to jupyterlab"
-    jupyter-lab --ip 0.0.0.0 --port 8000 --allow-root --notebook-dir=/home --ServerApp.token='' --ServerApp.password=''
+
+    # Run JupyterLab as odsp user
+    sudo -u odsp bash -c "jupyter-lab --ip 0.0.0.0 --port 8000 --allow-root --notebook-dir=/home --ServerApp.token='' --ServerApp.password=''"
 
 # Check if the value of JUPYTER_ENV is "jupyterhub"
 elif [ "$JUPYTER_ENV" = "jupyterhub" ]; then
     echo "JUPYTER_ENV is set to jupyterhub"
-    adduser --disabled-password --gecos "" odsp
-    adduser odsp sudo
-    
-    # Allow root and odsp users to run sudo commands without password
-    echo "root ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/root
-    echo "odsp ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/odsp
-    
-    # Add MLFLOW_TRACKING_URI and DASK_SCHEDULER_ADDRESS to odsp user
-    echo "export MLFLOW_TRACKING_URI=http://mlflow:2244" >> /home/odsp/.bashrc
-    echo "export DASK_SCHEDULER_ADDRESS=http://dask-scheduler:8786" >> /home/odsp/.bashrc
-    
+
+    # Run JupyterHub as root user
     jupyterhub -f /srv/jupyterhub/jupyterhub_config.py --ip 0.0.0.0
 
 # Else, the value is not understood
