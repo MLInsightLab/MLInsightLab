590eacd2d506c52d97a84e9340ee94ec0d6cb268
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-04-30 14:53:20 -0400

    adding basic authentication using nginx
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

---
+++ nginx/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if the .htpasswd file exists
+if [ ! -f /etc/nginx/.htpasswd ]; then
+    # Generate .htpasswd file
+    htpasswd -b -c /etc/nginx/.htpasswd $AUTH_USERNAME $AUTH_PASSWORD
+    echo "Password successfully configured"
+fi
+
+echo "Starting nginx"
+nginx -g 'daemon off;'
