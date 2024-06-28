4bfc2c90dc03182d7a8f56c6ac9a72b74b4a32ba
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-06-27 22:48:32 -0400

    finished password authentication to jupyter
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/create_password.py
+++
@@ -1,6 +0,0 @@
-from jupyter_server.auth import passwd
-import os
-
-password = passwd(os.environ['JUPYTER_PASSWORD'])
-with open(os.environ['PASSWORD_FILE'], 'w') as f:
-    f.write(password)
--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -13,10 +13,11 @@ echo "$NB_USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$NB_USER
 # Add environment variables
 echo "MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" >> /etc/environment
 echo "DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" >> /etc/environment
+echo "JUPYTER_PASSWORD=$JUPYTER_PASSWORD" >> /etc/environment
 
-# Ensure password is correctly written and created
-python /code/create_password.py
-password=$(cat $PASSWORD_FILE)
+# Copy the jupyter lab configuration file over to the user's home directory
+mkdir /home/$NB_USER/.jupyter
+cp /code/jupyter_lab_config.py /home/$NB_USER/.jupyter/jupyter_lab_config.py
 
 # Start Jupyter Lab under the NB_USER
-/bin/su --login -c "jupyter lab --notebook-dir=/home --ip=0.0.0.0 --port 8000 --no-browser --ServerApp.token='' --ServerApp.password=$password --allow-root" $NB_USER
+/bin/su --login -c "jupyter lab --notebook-dir=/home --ip=0.0.0.0 --port 8000 --no-browser --ServerApp.token='' --allow-root" $NB_USER
