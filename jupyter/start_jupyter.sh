ab07a3e8716368de335c8ceb8992e5c32f6f35a7
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-07-22 13:32:01 -0400

    adding read and write for everyone in the notebooks directory
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -4,5 +4,8 @@
 echo "export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" >> /etc/profile.d/mlflow.sh
 echo "export DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" >> /etc/profile.d/dask.sh
 
+# Add read and write permissions to /notebooks
+chmod -R +rw /notebooks
+
 # Start JupyterHub
 jupyterhub -f /srv/jupyter/jupyterhub_config.py --ip 0.0.0.0
