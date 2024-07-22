c261733f0f59bd05538d32d7bccfbff3a5b61667
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-07-22 12:52:46 -0400

    adding read and write permissions for users within the home directory
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -4,5 +4,9 @@
 echo "export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" >> /etc/profile.d/mlflow.sh
 echo "export DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" >> /etc/profile.d/dask.sh
 
+# Assign read and write rights to all folders within the /home directory
+chmod -R +r /home
+chmod -R +w /home
+
 # Start JupyterHub
 jupyterhub -f /srv/jupyter/jupyterhub_config.py --ip 0.0.0.0
