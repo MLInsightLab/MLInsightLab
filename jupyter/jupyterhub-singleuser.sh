b370fdf4cec4cbcb17a3a8e9763b59da1f881fe9
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-08-04 18:46:35 -0400

    adding API URL to user environments
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/jupyterhub-singleuser.sh
+++ jupyter/jupyterhub-singleuser.sh
@@ -2,5 +2,6 @@
 
 source /etc/profile.d/dask.sh
 source /etc/profile.d/mlflow.sh
+source /etc/profile.d/api.sh
 
 exec jupyterhub-singleuser
--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Add environment variables
-echo "export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" >> /etc/profile.d/mlflow.sh
-echo "export DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" >> /etc/profile.d/dask.sh
+echo "export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" > /etc/profile.d/mlflow.sh
+echo "export DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" > /etc/profile.d/dask.sh
+echo "export API_URL=$API_URL" > /etc/profile.d/api.sh
 
 # Add read and write permissions to /notebooks
 chmod 1777 /notebooks
