f40b0702a6b5edbdfb304213ef3e4e6462e09d90
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-08-09 10:25:40 -0400

    adding ssl and non-ssl versions (first pass)
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "Starting nginx"
+# Check for an environment variable that determines SSL mode
+if [ "$USE_SSL" == "true" ]; then
+    echo "Starting Nginx with SSL configuration"
+    # Copy or link the SSL configuration file to the default location
+    cp /etc/nginx/nginx.ssl.conf /etc/nginx/nginx.conf
+else
+    echo "Starting Nginx with Non-SSL configuration"
+    # Copy or link the Non-SSL configuration file to the default location
+    cp /etc/nginx/nginx.nonssl.conf /etc/nginx/nginx.conf
+fi
+
+echo "Starting Nginx"
 nginx -g 'daemon off;'
