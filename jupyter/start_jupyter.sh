dd34d93325a3c70982d854e84ff43243f7bf6e31
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2024-09-11 19:34:11 -0400

    changing modification parameters for /data
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- jupyter/start_jupyter.sh
+++ jupyter/start_jupyter.sh
@@ -5,8 +5,9 @@ echo "export MLFLOW_TRACKING_URI=$MLFLOW_TRACKING_URI" > /etc/profile.d/mlflow.s
 echo "export DASK_SCHEDULER_ADDRESS=$DASK_SCHEDULER_ADDRESS" > /etc/profile.d/dask.sh
 echo "export API_URL=$API_URL" > /etc/profile.d/api.sh
 
-# Add read and write permissions to /notebooks
+# Add read and write permissions to /notebooks and /data
 chmod 1777 /notebooks
+chmod 1777 /data
 
 # Start the cron service
 service cron restart
