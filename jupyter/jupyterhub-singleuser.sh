0cccae5cc961dafe8d7e21907ae99c4bb9cec11b
Jacob Renn <jacob.renn4@gmail.com>
2024-12-03 11:52:34 -0500

    fully removing all build code (moved to other repos) and build logic in docker-compose.*.*.yaml files
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- jupyter/jupyterhub-singleuser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-source /etc/profile.d/dask.sh
-source /etc/profile.d/mlflow.sh
-source /etc/profile.d/api.sh
-
-exec jupyterhub-singleuser
--- mlinsightlab/src/mlinsightlab/utils.py
+++
@@ -1,6 +0,0 @@
-from .MLILException import MLILException
-from .endpoints import *
-import pandas as pd
-import requests
-
-# TODO - function to test platform health & status
