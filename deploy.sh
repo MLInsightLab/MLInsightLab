4ead7fab036314bac8c0921f101076c7bc10d904
Jacob Renn <jacob.renn4@gmail.com>
2025-05-13 10:18:41 -0400

    separating docker compose files for easier development and deployment
    Signed-off-by: Jacob Renn <jacob.renn4@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Base compose file
+BASE_COMPOSE="docker-compose.base.yaml"
+
 # Pull the model deployment container image
 printf "📦 Pulling the latest model container image...\n\n"
 docker pull ghcr.io/mlinsightlab/mlinsightlab-model-container:latest
@@ -7,31 +10,27 @@ docker pull ghcr.io/mlinsightlab/mlinsightlab-model-container:latest
 # Determine whether to use SSL
 if [ -d "certs" ]; then
     printf "\n🔐 SSL certificates directory found. SSL deployment enabled.\n\n"
-    USE_SSL="true"
+    NGINX_COMPOSE="docker-compose.ssl.yaml"
+    
 else
     printf "\n⚠️  SSL certificates directory not found. Proceeding with non-SSL deployment.\n\n"
-    USE_SSL="false"
+    NGINX_COMPOSE="docker-compose.nonssl.yaml"
 fi
 
 # Check for GPU availability
 if command -v nvidia-smi > /dev/null && nvidia-smi -L > /dev/null; then
+    
     printf "🖥️  GPU detected. Deploying with GPU support...\n\n"
+    GPU_COMPOSE="docker-compose.gpu.yaml"
 
-    if [ "$USE_SSL" = "true" ]; then
-	    docker compose -f docker-compose.ssl.gpu.yaml pull
-        docker compose -f docker-compose.ssl.gpu.yaml up -d
-    else
-	    docker compose -f docker-compose.nonssl.gpu.yaml pull
-        docker compose -f docker-compose.nonssl.gpu.yaml up -d
-    fi
+# GPU not detected
 else
     printf "🚫 No GPU detected or NVIDIA drivers missing. Deploying CPU-only version...\n\n"
-
-    if [ "$USE_SSL" = "true" ]; then
-	    docker compose -f docker-compose.ssl.nongpu.yaml pull
-        docker compose -f docker-compose.ssl.nongpu.yaml up -d
-    else
-    	docker compose -f docker-compose.nonssl.nongpu.yaml pull
-        docker compose -f docker-compose.nonssl.nongpu.yaml up -d
-    fi
+    GPU_COMPOSE="docker-compose.nongpu.yaml"
 fi
+
+# Pull the containers
+docker compose -f ${BASE_COMPOSE} -f ${NGINX_COMPOSE} -f ${GPU_COMPOSE} pull
+
+# Deploy
+docker compose -f ${BASE_COMPOSE} -f ${NGINX_COMPOSE} -f ${GPU_COMPOSE} up -d
--- teardown.sh
+++ teardown.sh
@@ -1,33 +1,27 @@
 #!/bin/sh
 
+# Base compose file
+BASE_COMPOSE="docker-compose.base.yaml"
+
 # Determine whether to tear down SSL version
 if [ -d "certs" ]; then
     printf "\n🔐 SSL certificates directory found. SSL teardown initiated.\n\n"
-    USE_SSL="true"
+    NGINX_COMPOSE="docker-compose.ssl.yaml"
 else
     printf "\n⚠️  SSL certificates directory not found. Proceeding with non-SSL teardown.\n\n"
-    USE_SSL="false"
+    NGINX_COMPOSE="docker-compose.nonssl.yaml"
 fi
 
 # Check for GPU availability
-if command -v nvidia-smi > /dev/null && nvidia-smi -L > /dev/null; then
+if command -v nvidia-smi > /dev/null && nvidia-smi -L > /dev/null; then    
     printf "🖥️  GPU detected. Tearing down GPU version...\n\n"
-
-    # Tear down SSL or non-SSL version appropriately
-    if [ "$USE_SSL" = "true" ]; then
-        docker compose -f docker-compose.ssl.gpu.yaml down
-    else
-        docker compose -f docker-compose.nonssl.gpu.yaml down
-    fi
+    GPU_COMPOSE="docker-compose.gpu.yaml"
 
 # GPU not detected - tear down CPU only
 else
-    echo "🚫 No GPU detected or NVIDIA drivers missing. Tearing down CPU-only version...\n\n"
-
-    # Tear down SSL or non-SSL version appropriately
-    if [ "$USE_SSL" = "true" ]; then
-        docker compose -f docker-compose.ssl.nongpu.yaml down
-    else
-        docker compose -f docker-compose.nonssl.nongpu.yaml down
-    fi
+    printf "🚫 No GPU detected or NVIDIA drivers missing. Tearing down CPU-only version...\n\n"
+    GPU_COMPOSE="docker-compose.nongpu.yaml"
 fi
+
+# Tear down the services
+docker compose -f ${BASE_COMPOSE} -f ${NGINX_COMPOSE} -f ${GPU_COMPOSE} down
